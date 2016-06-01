7e88924bd8d64ec21a7c9b96a244c9c1564bc421
Jonathan Arnett <jonarnett90@gmail.com>
2016-06-01 10:41:16 -0400

    Added reciprocal relation between users and battle

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :battles, foreign_key: 'created_by_id'
 end
