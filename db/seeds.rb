a954c2d8a41af2bfbdc35d2acd70cf1ffe6bc66b
Jonathan Arnett <jonarnett90@gmail.com>
2016-06-07 09:34:53 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([
+  { email: "jonathan.arnett@example.com", password: "password", full_name: "Jonathan Arnett" },
+  { email: "austin.trout@example.com",    password: "password", full_name: "Austin Trout" },
+  { email: "dimitry.raymond@example.com", password: "password", full_name: "Dimitry Raymond" },
+  { email: "ethan.fetsko@example.com",    password: "password", full_name: "Ethan Fetsko" },
+  { email: "george.milberger@example.com",password: "password", full_name: "George Milberger" },
+])
+
+battle = Battle.create!(topic: "Work Shenanigans", description: "Post-ironic selfies franzen, YOLO sriracha vice helvetica squid salvia tofu blog beard fanny pack +1.", created_by: User.all.sample)
+
+comment = Comment.create!(user: User.all.sample, battle: battle, body: "http://www.gifbin.com/bin/012016/beer-on-speeding-tank-39-s-cannon.gif")
+sub_comment = Comment.create!(user: User.all.sample, battle: battle, body: "Neat gif!", comment: comment)
+Comment.create!(user: User.all.sample, battle: battle, body: "Cheers!", comment: sub_comment)
+
+other_comment = Comment.create!(user: User.all.sample, battle: battle, body: "http://www.picgifs.com/movies-and-series/movies/indiana-jones/picgifs-indiana-jones-1667336.gif")
+Comment.create!(user: User.all.sample, battle: battle, body: "I love this film!", comment: other_comment)
+
+battle.update(winning_comment: comment)
+
+Battle.create!(topic: "In a hurry", description: "Blog pinterest pitchfork cliche, chia plaid letterpress aesthetic keytar bicycle rights keffiyeh selvage church-key narwhal.", created_by: User.all.sample)
