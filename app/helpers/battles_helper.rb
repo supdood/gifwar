2f70bdd1f838c2617369bf8a6566bc48e385ad15
Ethan Fetsko <ethanfetsko@ml-r1fv2scv548o.bluefishwireless.net>
2016-06-08 18:01:20 -0400

    Made improvements to both video and image elements in the battle index view and induvidual battle views

--- app/helpers/battles_helper.rb
+++ app/helpers/battles_helper.rb
@@ -1,11 +1,16 @@
 module BattlesHelper
-  def text_with_images(text)
+  def text_with_images(text, flex=false)
     video_regex = /https?:\/\/\S+\.(?:gifv|webm|mp4)/i
     image_regex = /https?:\/\/\S+\.(?:gif|jpg|jpeg|png)/i
     text.split(/(?<=\s)/).map do |match|
       if match.match(video_regex)
-        substitution = match.sub(/gifv$/, "webm")
-        "<br>#{video_tag(substitution, autoplay:"autoplay", loop:"loop")}<br>"
+        if flex
+          substitution = match.sub(/gifv$/, "webm")
+          "<div class='flex-video'>#{video_tag(substitution, autoplay:"autoplay", loop:"loop")}</div>"
+        else
+          substitution = match.sub(/gifv$/, "webm")
+          "<br>#{video_tag(substitution, autoplay:"autoplay", loop:"loop")}<br>"
+        end
       elsif match.match(image_regex)
         "<br>#{image_tag(match)}<br>"
       else
